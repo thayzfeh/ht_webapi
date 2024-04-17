7ed6586cad90dbd9e0625e2b393a61bd36ea97d2
Thaís Ferreira <110707634+thayzfeh@users.noreply.github.com>
2024-04-17 11:00:03 -0300

    teste debug gpt

--- routes/showConnection.js
+++ routes/showConnection.js
@@ -2,11 +2,18 @@ const Connection = require("../models/Connection");
 const User = require("../models/User");
 const userById = require("../selects/userById");
 
-module.exports = (req, res) =>{
-    sender = req.sender.map(async(x) =>{
-        console.log('showConnection map', x);
-        return await userById(x.receptor_id);
-    })
-    console.log(sender)
-    res.status(200).json({sender, receptor : req.receptor})
+module.exports = async(req, res) =>{
+    try {
+        const senderPromises = req.sender.map(async (x) => {
+            console.log('showConnection map', x);
+            return await userById(x.receptor_id);
+        });
+
+        const sender = await Promise.all(senderPromises); // Wait for all promises to resolve
+
+        console.log(sender);
+        res.status(200).json({ sender, receptor: req.receptor });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 }
--- selects/userById.js
+++ selects/userById.js
@@ -4,17 +4,16 @@ const User = require("../models/User")
 
 
 module.exports = async(id) =>{
-    const select = new Promise(async(resolve, reject) =>{
-        
+    try {
         console.log("userByID id", id);
         
         const user = await User.findById(id);
         
         console.log('userByID user', user);
         
-        resolve(user)
-
-        console.log('userByID resolve promise ', resolve(user));
-    })
-    return await select.then((x) => x.data)
+        return user;
+    } catch (error) {
+        console.error("Error in userById:", error);
+        throw error;
+    }
 }
