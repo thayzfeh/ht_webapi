1268f92ced05709e9a75086f72f70fc15ec17ccf
Thaís Ferreira <110707634+thayzfeh@users.noreply.github.com>
2024-04-17 11:19:48 -0300

    adição exclude params senderPromises endpoint showConnection

--- routes/showConnection.js
+++ routes/showConnection.js
@@ -1,18 +1,16 @@
-const Connection = require("../models/Connection");
-const User = require("../models/User");
 const userById = require("../selects/userById");
 
 module.exports = async(req, res) =>{
     try {
         const senderPromises = req.sender.map(async (x) => {
-            return await userById(x.receptor_id);
+            return await userById(x.receptor_id, "-_id -cpf -password -__v");
         });
         const receptorPromises = req.receptor.map(async(x) =>{
             return await userById(x.sender_id);
         })
         const sender = await Promise.all(senderPromises);
         const receptor = await Promise.all(receptorPromises);
-        
+
         res.status(200).json({ sender, receptor});
     } catch (error) {
         res.status(500).json({ error: error.message });
--- selects/userById.js
+++ selects/userById.js
@@ -3,9 +3,9 @@ const User = require("../models/User")
 
 
 
-module.exports = async(id) =>{
+module.exports = async(id, exclude = "") =>{
     try {
-        const user = await User.findById(id);  
+        const user = await User.findById(id, exclude);  
         return user;
     } catch (error) {
         console.error("Error in userById:", error);
