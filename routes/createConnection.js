2389fa491ad84888f666d9eefb564806384cd1b9
Thaís Ferreira <110707634+thayzfeh@users.noreply.github.com>
2024-04-17 11:26:47 -0300

    teste condicional conexão existente endpoint createConnection

--- routes/createConnection.js
+++ routes/createConnection.js
@@ -5,17 +5,21 @@ module.exports = async(req, res) =>{
     const {receptor} = req.body;
     const sender = req.decodedToken.id;
     
-    if(!receptor){
-        return res.status(422).json({msg: 'O receptor é obrigatório!'});
-    }
+    if(!receptor) return res.status(422).json({msg: 'O receptor é obrigatório!'});
 
     const receptionUser = await User.findOne({username : receptor});
-    if(!receptionUser){
-        return res.status(404).json({msg: 'Usuário não encontrado!'});
-    }
+    if(!receptionUser) return res.status(404).json({msg: 'Usuário não encontrado!'});
+
 
     const receptor_id = receptionUser._id;
 
+    const hasConnection = await Connection.findOne({
+        sender_id : sender,
+        receptor_id : receptor_id
+    });
+    if(hasConnection) return res.status(500).json({msg: 'Conexão já existente!'})
+
+    
     const connection = new Connection({
         sender_id : sender,
         receptor_id
