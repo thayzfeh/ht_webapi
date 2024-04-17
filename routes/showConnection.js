b5bc2d7175cd54ef92120f36bc7e92954dfddf0e
Thaís Ferreira <110707634+thayzfeh@users.noreply.github.com>
2024-04-17 10:48:43 -0300

    teste debug2

--- routes/showConnection.js
+++ routes/showConnection.js
@@ -3,9 +3,9 @@ const User = require("../models/User");
 const userById = require("../selects/userById");
 
 module.exports = (req, res) =>{
-    sender = req.sender.map((x) =>{
+    sender = req.sender.map(async(x) =>{
         console.log('showConnection map', x);
-        return userById(x.receptor_id);
+        return await userById(x.receptor_id);
     })
     console.log(sender)
     res.status(200).json({sender, receptor : req.receptor})
